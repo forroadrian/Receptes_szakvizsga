a30edd1d40d6f71256afd9e866adce2d2124c59e
KrizsanMilan <krizsan.milan.2021i@bankitatabanya.hu>
2026-02-24 01:01:39 +0100

    Created and Card basics

---
+++ app/components/Card.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    const props = defineProps({
+        orientation: String
+    })
+
+    const cardDirection = computed(() => {
+        if (props.orientation == "vertical"){
+            return "flex-column c-card--vertical"
+        }
+        return "flex-row c-card--horizontal"
+        
+    })
+
+</script>
+
+<template>
+    <div class="c-card-base d-flex" :class="cardDirection">
+        <slot></slot>
+    </div>
+</template>
---
+++ app/components/CardTitle.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+export type HeadingRank = 1 | 2 | 3 | 4 | 5 | 6
+
+const props = withDefaults(
+  defineProps<{ rank?: HeadingRank }>(),
+  { rank: 2 }
+)
+
+const headingTag = computed<`h${HeadingRank}`>(() => `h${props.rank}` as const)
+</script>
+<template>
+  <component :is="headingTag" class="c-card--title">
+    <slot />
+  </component>
+</template>
