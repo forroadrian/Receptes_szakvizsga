c1d02cb0a5720d1201bc4f4bf6eae99205b9ff87
KrizsanMilan <krizsan.milan.2021i@bankitatabanya.hu>
2026-03-11 21:30:34 +0100

    worked on ingredient page design

--- app/composables/useSearch.ts
+++ app/composables/useSearch.ts
@@ -2,6 +2,7 @@ import type SearchParams from "~/interfaces/SearchParams";
 
 export const useSearch = <T extends object>(params: Ref<SearchParams<T>>) => {
     return computed(() => {
+        if (params.value.showAllByDefault && (params.value.query?.length == 0 || params.value.query == undefined)) return params.value.haystack;
         return params.value.haystack.filter((element) => {
             return params.value.searchFor.some((key) => {
                 const value = getValue(element, key as string);
