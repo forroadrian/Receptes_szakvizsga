1729aa58bc3df0fabfa744199f3a1d12f2bb3b8e
KrizsanMilan <90619390+KrizsanMilan@users.noreply.github.com>
2026-03-12 13:05:40 +0100

    supabase connection

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,12 +7,18 @@ export default defineNuxtConfig({
         '~/assets/css/main.css',
         'bootstrap-icons/font/bootstrap-icons.css'
     ],
-    modules: ['@nuxtjs/color-mode', '@nuxt/image'],
+    modules: ['@nuxtjs/color-mode', '@nuxt/image', '@pinia/nuxt'],
     colorMode: {
         preference: 'light',
         fallback: 'light',
         dataValue: 'bs-theme',
         storage: 'cookie',
         storageKey: 'nuxt-color-mode'
+    },
+    runtimeConfig: {
+        public: {
+            supabaseUrl: process.env.SUPABASE_URL,
+            supabasePublishableKey: process.env.SUPABASE_PUBLISHABLE_KEY,
+        }
     }
 })
