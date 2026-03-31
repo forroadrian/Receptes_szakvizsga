4341e0ed43aaee4135d25b5a2e993c0bfa2d7211
forroadrian <94844821+forroadrian@users.noreply.github.com>
2026-03-31 23:34:32 +0200

    update profile page mobile layout and cards

--- app/components/profile/ProfileSettingsOption.vue
+++ app/components/profile/ProfileSettingsOption.vue
@@ -10,17 +10,28 @@ defineProps({
         type: String,
         required: true,
     },
+    description: {
+        type: String,
+        default: '',
+    },
 });
 </script>
 
 <template>
     <button type="button" class="settings-option" @click="$emit('click')">
         <div class="settings-option-left">
-            <i class="bi" :class="icon"></i>
-            <div>
+            <div class="settings-option-icon">
+                <i class="bi" :class="icon"></i>
+            </div>
+
+            <div class="settings-option-text">
                 <h5 class="mb-1">{{ title }}</h5>
+                <p v-if="description" class="mb-0">{{ description }}</p>
             </div>
         </div>
-        <i class="bi bi-chevron-right"></i>
+
+        <div class="settings-option-arrow">
+            <i class="bi bi-arrow-right"></i>
+        </div>
     </button>
 </template>
